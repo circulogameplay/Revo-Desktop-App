05c1b5f3104b53b92e59232415833f8e68d1411b
João Victor Madureira <circulogameplay@gmail.com>
2021-10-03 15:54:45 -0300

    primeiro commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+(function($) {
+    "use strict";
+
+    $(window).on('scroll load', function() {
+        if ($(".navbar").offset().top > 60) {
+            $(".fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".fixed-top").removeClass("top-nav-collapse");
+        }
+    });
+
+    $
+})
